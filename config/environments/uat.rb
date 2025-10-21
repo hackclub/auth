d66674ce4a222c3916aac4becfc3b9349651ecb6
24c02 <163450896+24c02@users.noreply.github.com>
2025-10-21 19:16:50 -0400

    we ARE

--- config/environments/uat.rb
+++ config/environments/uat.rb
@@ -80,6 +80,8 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
+  config.are_we_enterprise_yet = true
+  
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
