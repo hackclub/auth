f4e04ba9ba9a120ddc101da853dc5e1c4df3b6d8
24c02 <163450896+24c02@users.noreply.github.com>
2025-10-21 20:17:02 -0400

    darn it

--- app/models/onboarding_scenarios/default_join.rb
+++ app/models/onboarding_scenarios/default_join.rb
@@ -10,9 +10,9 @@ module OnboardingScenarios
 
 
         def slack_user_type = :multi_channel_guest
-        
-        def slack_channels = Rails.configuration.slack_channels.slice(:announcements)
 
-        def promotion_channels = Rails.configuration.slack_channels.slice(:welcome, :ship, :neighbourhood, :library, :lounge)
+        def slack_channels = Rails.configuration.slack_channels.slice(:announcements).keys
+
+        def promotion_channels = Rails.configuration.slack_channels.slice(:welcome, :ship, :neighbourhood, :library, :lounge).keys
     end
 end
