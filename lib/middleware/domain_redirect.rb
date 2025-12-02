ba38578d4a0fd1d2e5a67c63fd835bc40a7c694f
24c02 <163450896+24c02@users.noreply.github.com>
2025-12-02 04:47:23 -0500

    hotfix old auth url

--- lib/middleware/domain_redirect.rb
+++ lib/middleware/domain_redirect.rb
@@ -6,7 +6,7 @@ class DomainRedirect
   def call(env)
     request = Rack::Request.new(env)
 
-    if request.path.start_with?("/api") || request.host == "auth.hackclub.com"
+    if request.path.start_with?("/api") || request.path.start_with?("/oauth") || request.host == "auth.hackclub.com"
       return @app.call(env)
     end
 
