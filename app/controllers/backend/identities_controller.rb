2527bb19bee7317a4e072852d72dce1c9fe80d66
24c02 <163450896+24c02@users.noreply.github.com>
2025-12-02 22:44:47 -0500

    styles, BACKSPACE

--- app/controllers/backend/identities_controller.rb
+++ app/controllers/backend/identities_controller.rb
@@ -5,10 +5,13 @@ module Backend
     hint :list_navigation, on: :index
     hint :search_focus, on: :index
     hint :pagination, on: :index
+    hint :back_navigation, on: :index
     hint :identity_actions, on: :show
 
     def index
       authorize Identity
+
+      set_keyboard_shortcut(:back, backend_root_path)
 
       if (search = params[:search])&.start_with? "ident!"
         ident = Identity.find_by_public_id(search)
