83eaa8b81efb98a51118a2320efdcfbab5a820d6
24c02 <163450896+24c02@users.noreply.github.com>
2025-12-02 23:13:58 -0500

    lint pass

--- app/controllers/backend/users_controller.rb
+++ app/controllers/backend/users_controller.rb
@@ -39,9 +39,9 @@ module Backend
 
     def create
       authorize User
-      
+
       identity = Identity.find(params[:identity_id])
-      
+
       if User.exists?(identity_id: identity.id)
         redirect_to backend_users_path, alert: "This identity already has backend access!"
         return
