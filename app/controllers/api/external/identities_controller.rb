5e5acb72bb8a719226e514347d49e4747f1483a9
24c02 <163450896+24c02@users.noreply.github.com>
2025-12-15 14:27:56 -0500

    unnecessary

--- app/controllers/api/external/identities_controller.rb
+++ app/controllers/api/external/identities_controller.rb
@@ -4,41 +4,36 @@ module API
       before_action :set_cors_headers, only: %i[check options]
 
       def check
-        ident =
-          if (public_id = params[:idv_id]).present?
-            Identity.find_by_public_id(public_id)
-          elsif (primary_email = params[:email]).present?
-            Identity.find_by(primary_email:)
-          elsif (slack_id = params[:slack_id]).present?
-            Identity.find_by(slack_id:)
-          else
-            raise ActionController::ParameterMissing,
-                  "provide one of: idv_id, email, slack_id"
-          end
+        ident = if (public_id = params[:idv_id]).present?
+                  Identity.find_by_public_id(public_id)
+        elsif (primary_email = params[:email]).present?
+                  Identity.find_by(primary_email:)
+        elsif (slack_id = params[:slack_id]).present?
+                  Identity.find_by(slack_id:)
+        else
+                  raise ActionController::ParameterMissing, "provide one of: idv_id, email, slack_id"
+        end
 
-        result =
-          if ident
-            case ident.verification_status
-            when "needs_submission", "pending"
-              ident.verification_status
-            when "verified"
-              ident.ysws_eligible? ? "verified_eligible" : "verified_but_over_18"
-            when "ineligible"
-              "rejected"
-            else
-              "unknown"
-            end
-          else
-            "not_found"
-          end
-
-        render json: { result: }
+        result = if ident
+                   case ident.verification_status
+                   when "needs_submission", "pending"
+                     ident.verification_status
+                   when "verified"
+                     ident.ysws_eligible? ? "verified_eligible" : "verified_but_over_18"
+                   when "ineligible"
+                     "rejected"
+                   else
+                     "unknown"
+                   end
+        else
+                   "not_found"
+        end
+        render json: {
+          result:
+        }
       end
 
-      # Handles CORS preflight
-      def options
-        head :ok
-      end
+      def options = head :ok
 
       private
 
