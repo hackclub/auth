06a617f6ec684365f367de7c3830854a57a265d5
24c02 <163450896+24c02@users.noreply.github.com>
2025-12-17 18:11:42 -0500

    logotown

--- app/models/onboarding_scenarios/flavortown.rb
+++ app/models/onboarding_scenarios/flavortown.rb
@@ -71,7 +71,7 @@ module OnboardingScenarios
       end
     end
 
-    def logo_path = "images/flavortown/logo.avif"
+    def logo_path = "images/flavortown/flavortown.png"
     def background_path = "images/flavortown/hero-bg.webp"
     def dark_mode_background_path = "images/flavortown/bg-dark.png"
   end
