fd996cf867c6e75a635345029013155f7956bb35
24c02 <163450896+24c02@users.noreply.github.com>
2025-12-18 13:21:02 -0500

    fix addr portal start action

--- app/controllers/concerns/portal_flow.rb
+++ app/controllers/concerns/portal_flow.rb
@@ -2,8 +2,8 @@ module PortalFlow
   extend ActiveSupport::Concern
 
   included do
-    before_action :validate_portal_return_url, only: [ :start, :portal ]
-    before_action :store_return_url, only: [ :start, :portal ]
+    before_action :validate_portal_return_url, only: [ :portal ]
+    before_action :store_return_url, only: [ :portal ]
     helper_method :portal_onboarding_scenario
   end
 
