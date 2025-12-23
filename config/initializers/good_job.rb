506bd073da4ff2ec1997366f1e8206d888cd2c15
24c02 <163450896+24c02@users.noreply.github.com>
2025-12-23 14:10:26 -0500

    add rails_pulse crons

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -3,6 +3,14 @@ Rails.application.configure do
     expire_draft_aadhaar_verifications: {
       cron: "*/5 * * * *", # Run every 5 minutes
       class: "Verification::ExpireDraftAadhaarVerificationsJob"
+    },
+    rails_pulse_summary: {
+      cron: "5 * * * *", # Run 5 minutes past every hour
+      class: "RailsPulse::SummaryJob"
+    },
+    rails_pulse_cleanup: {
+      cron: "0 1 * * *", # Run daily at 1:00 AM
+      class: "RailsPulse::CleanupJob"
     }
   }
 end
