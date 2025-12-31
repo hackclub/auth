c645d9538f517af7743c7b5c9468eb410ced37a8
24c02 <163450896+24c02@users.noreply.github.com>
2025-12-30 20:28:04 -0500

    waow

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -12,6 +12,7 @@ Sentry.init do |config|
   config.send_default_pii = true
   config.traces_sample_rate = Rails.env.production? ? 0.1 : 1.0
   config.enabled_patches = [ :http, :redis, :puma ]
+  config.sdk_logger = Rails.logger
 
   # Capture error event IDs in Rack env for error pages
   config.rails.report_rescued_exceptions = true
