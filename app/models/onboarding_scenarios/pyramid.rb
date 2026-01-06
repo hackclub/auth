a1af8d6bc37f0e9ec23c08b8561d434cb490d7df
Parth <215315028+nystar1@users.noreply.github.com>
2026-01-06 23:30:49 +0530

    new onboarding scenario for pyramid scheme

---
+++ app/models/onboarding_scenarios/pyramid.rb
@@ -0,0 +1,17 @@
+module OnboardingScenarios
+  class Pyramid < Base
+    def self.slug = "pyramid"
+
+    def title
+      "Join the Pyramid"
+    end
+
+    def form_fields
+      [ :first_name, :last_name, :primary_email, :birthday, :country ]
+    end
+
+    def accepts_adults = true
+
+    def next_action = :home
+  end
+end
