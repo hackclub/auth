7f1f59a72b97cdb520e7fb9168b4559c9a714c62
24c02 <163450896+24c02@users.noreply.github.com>
2026-01-13 16:45:34 -0500

    hasn't been a thing for a while

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -1,9 +1,9 @@
 Rails.application.configure do
   config.good_job.cron = {
-    expire_draft_aadhaar_verifications: {
-      cron: "*/5 * * * *", # Run every 5 minutes
-      class: "Verification::ExpireDraftAadhaarVerificationsJob"
-    },
+    # expire_draft_aadhaar_verifications: {
+    #   cron: "*/5 * * * *", # Run every 5 minutes
+    #   class: "Verification::ExpireDraftAadhaarVerificationsJob"
+    # },
     purge_old_analytics: {
       cron: "0 4 * * *", # Run daily at 4am
       class: "Analytics::PurgeOldDataJob"
