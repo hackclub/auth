9771258e7ca420ed0f52809d5339270f092e211c
EvanStreams <eps@hackclub.com>
2026-01-30 13:19:08 -0500

    New join default now SCG in #welcome-to-hack-club

--- app/models/onboarding_scenarios/slack_join.rb
+++ app/models/onboarding_scenarios/slack_join.rb
@@ -10,13 +10,13 @@ module OnboardingScenarios
       [ :first_name, :last_name, :primary_email, :birthday, :country ]
     end
 
-    def slack_user_type = :multi_channel_guest
+    def slack_user_type = :multi_single_guest
 
     def next_action = :slack
 
     def slack_onboarding_flow = :internal_tutorial
 
-    def slack_channels = Rails.configuration.slack_channels.slice(:waiting_room).values
+    def slack_channels = Rails.configuration.slack_channels.slice(:welcome_to_hack_club).values
 
     def promotion_channels = Rails.configuration.slack_channels.slice(:announcements, :welcome, :ship, :neighbourhood, :library, :lounge).values
   end
