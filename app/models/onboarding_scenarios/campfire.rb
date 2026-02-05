5f005cb4252da2e8ef41e837d786acf4fd4f2307
24c02 <163450896+24c02@users.noreply.github.com>
2026-02-05 14:39:03 -0500

    camp the fire?

--- app/models/onboarding_scenarios/campfire.rb
+++ app/models/onboarding_scenarios/campfire.rb
@@ -12,7 +12,9 @@ module OnboardingScenarios
 
     def slack_user_type = :multi_channel_guest
 
-    def slack_channels = chans(:campfire_flagship, :campfire_flagship_bulletin, :campfire_flagship_help)
+    def slack_channels = chans(:campfire_flagship, :campfire_flagship_bulletin, :campfire_flagship_help, :welcome_to_hack_club)
+
+    def slack_onboarding_flow = :internal_tutorial
 
     def next_action = :home
 
@@ -21,5 +23,12 @@ module OnboardingScenarios
 
     def card_attributes = { wide_logo: true }
     def dark_mode_background_path = "images/campfire/campfire.png"
+
+    def dialogue_flow
+      {
+        intro: { template: "tutorial/campfire/intro", next: :welcome },
+        welcome: { template: "tutorial/03_welcome", next: nil }
+      }
+    end
   end
 end
