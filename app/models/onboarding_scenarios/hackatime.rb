9a353188ad1257c00e6076c9c38194fb896be8ab
Mahad Kalam <mahadkalam1234@gmail.com>
2026-02-19 09:46:26 +0000

    Add em to the channels!

--- app/models/onboarding_scenarios/hackatime.rb
+++ app/models/onboarding_scenarios/hackatime.rb
@@ -18,7 +18,7 @@ module OnboardingScenarios
 
     def slack_user_type = :multi_channel_guest
     def slack_onboarding_flow = :internal_tutorial
-    def slack_channels = Rails.configuration.slack_channels.slice(:welcome_to_hack_club).values
+    def slack_channels = Rails.configuration.slack_channels.slice(:welcome_to_hack_club, :hackatime_dev, :hackatime_help).values
     def promotion_channels = Rails.configuration.slack_channels.slice(:announcements, :happenings, :community, :hardware, :code, :ship, :neighbourhood, :library, :lounge).values
     def send_ephemeral_in_channel? = true
     def ephemeral_channel = Rails.configuration.slack_channels[:welcome_to_hack_club]
