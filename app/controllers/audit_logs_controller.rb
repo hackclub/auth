b9a429a58ed54c8adebbe4db35ec1584b32f718f
End Nightshade <contact@endoftimee.tech>
2026-03-04 09:01:33 -0700

    fixes 196 & 197

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -3,6 +3,7 @@ class AuditLogsController < ApplicationController
     @activities = PublicActivity::Activity
       .where("(recipient_id = ? AND recipient_type = ?) OR (owner_id = ? AND owner_type = ?)",
              current_identity.id, "Identity", current_identity.id, "Identity")
+      .where.not(trackable_type: "Program")
       .order(created_at: :desc)
       .page(params[:page])
       .per(50)
