3c17fca9f0b835d89c3e7f2c9661fea708a4cb8e
ascpixi <44982772+ascpixi@users.noreply.github.com>
2026-03-16 20:05:14 -0400

    feat: add Hack Club: The Game

---
+++ app/models/onboarding_scenarios/game.rb
@@ -0,0 +1,24 @@
+module OnboardingScenarios
+  class Game < Base
+    def self.slug = "game"
+
+    def title = "Welcome to The Game!"
+
+    def form_fields
+      [ :first_name, :last_name, :primary_email, :birthday, :country ]
+    end
+
+    def slack_user_type = :multi_channel_guest
+
+    def slack_channels = chans(:hack_club_the_game, :hctg_bulletin, :hctg_help, :welcome_to_hack_club)
+
+    def slack_onboarding_flow = :internal_tutorial
+
+    def next_action = :home
+
+    def logo_path = "images/game/logo.png"
+    def background_path = "images/game/background.png"
+
+    def card_attributes = { wide_logo: true }
+  end
+end
