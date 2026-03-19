6c08b994762e756973a0c898a1e878758820641f
Sam Liu <sam@samliu.dev>
2026-03-19 17:05:51 -0400

    Add Fallout onboarding scenario model (#201)
    * Add Fallout onboarding scenario model
    
    * Add Fallout tutorial and Slack channel configurations
    
    * Update welcome image in Fallout tutorial message

---
+++ app/models/onboarding_scenarios/fallout.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module OnboardingScenarios
+  class Fallout < Base
+    def self.slug = "fallout"
+
+    def title = "let's build!"
+
+    def form_fields
+      [ :first_name, :last_name, :primary_email, :birthday, :country ]
+    end
+
+    def slack_user_type = :multi_channel_guest
+
+    def slack_channels = chans(:fallout, :fallout_help, :fallout_bulletin, :identity_help)
+
+    def slack_onboarding_flow = :internal_tutorial
+
+    def next_action = :home
+
+    def logo_path = "images/fallout/fallout.png"
+    def background_path = "images/fallout/bg-img.png"
+
+    def card_attributes = { wide_logo: true }
+    def dark_mode_background_path = "images/fallout/bg-img.png"
+
+    def dialogue_flow
+      {
+        intro: { template: "tutorial/fallout/intro", next: :welcome },
+        welcome: { template: "tutorial/fallout/03_welcome", next: nil }
+      }
+    end
+  end
+end
