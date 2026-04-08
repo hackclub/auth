1c9af6efd9b3217e03b6dbc24471126d6dd22c26
24c02 <163450896+24c02@users.noreply.github.com>
2026-04-07 20:09:13 -0400

    refactor policies?

--- app/policies/address_policy.rb
+++ app/policies/address_policy.rb
@@ -1,35 +1,37 @@
 class AddressPolicy < ApplicationPolicy
-  # user is either an Identity (public) or Backend::User (admin)
-  def show?    = owns? || super_admin?
-  def create?  = owns? || super_admin?
-  def update?  = owns? || super_admin?
-  def destroy? = owns? || super_admin?
+  include IdentityAware
+
+  def index?
+    true
+  end
+
+  def show?
+    owns? || super_admin?
+  end
+
+  def create?
+    owns? || super_admin?
+  end
+
+  def update?
+    owns? || super_admin?
+  end
+
+  def destroy?
+    owns? || super_admin?
+  end
 
   class Scope < ApplicationPolicy::Scope
+    include IdentityAware
+
     def resolve
-      if user.is_a?(Identity)
-        scope.where(identity: user)
-      elsif user.is_a?(Backend::User) && user.super_admin?
+      if super_admin?
         scope.all
       else
-        scope.none
+        scope.where(identity: user)
       end
     end
   end
 
   private
-
-  def owns?
-    user.is_a?(Identity) && record.identity == user
-  end
-
-  def super_admin?
-    if user.is_a?(Backend::User)
-      user.super_admin?
-    elsif user.is_a?(Identity)
-      user.backend_user&.super_admin?
-    else
-      false
-    end
-  end
 end
