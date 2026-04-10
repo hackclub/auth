2c42ec55036dfc3e25e909b6d5b51d763c61d663
ascpixi <44982772+ascpixi@users.noreply.github.com>
2026-04-10 12:11:40 -0400

    Add Hack Club: The Game to homepage app grid

---
+++ app/models/special_app_cards/game.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module SpecialAppCards
+  class Game < Base
+    def visible?
+      Flipper.enabled?(:game, identity)
+    end
+
+    def friendly_name = "Hack Club: The Game"
+
+    def tagline = "Build projects, then compete in a scavenger hunt adventure game across Manhattan"
+
+    def icon = "game.png"
+
+    def url = "https://game.hackclub.com"
+
+    def launch_text = "Play Now!"
+  end
+end
