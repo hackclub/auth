152623fe1cb753b0027ac8dae5ea07234841e2ca
mani <227445662+0xmanidev@users.noreply.github.com>
2026-05-12 01:15:31 +0530

    Create forge.rb

---
+++ app/models/special_app_cards/forge.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+module SpecialAppCards
+  class Forge < Base
+    def visible?
+      identity.ysws_eligible != false
+    end
+    def friendly_name = "Forge"
+    def tagline = "This is program where teens like yourself can design and build hardware projects, and get them funded!"
+    def icon = "Forge.png"
+    def icon_background = "#0E305B"
+    def url = "https://forge.hackclub.com/"
+    def launch_text = "Get forging now"
+  end
+end
