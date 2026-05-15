62fcfb55d07bc019de4ea20b5081289ab2105b87
Leo Wilkin <leo@wilkin.xyz>
2026-05-15 16:40:43 +0100

    Store Sentry event_id (not full event) in flash

--- app/controllers/backend/application_controller.rb
+++ app/controllers/backend/application_controller.rb
@@ -96,14 +96,14 @@ module Backend
     end
 
     rescue_from Pundit::NotAuthorizedError do |e|
-      event_id = Sentry.capture_exception(e)
+      event_id = Sentry.capture_exception(e)&.event_id
       flash[:error] = "you don't seem to be authorized to do that?"
       flash[:sentry_event_id] = event_id if event_id
       redirect_to backend_root_path unless request.path == "/backend" || request.path == "/backend/"
     end
 
     rescue_from ActiveRecord::RecordNotFound do |e|
-      event_id = Sentry.capture_exception(e)
+      event_id = Sentry.capture_exception(e)&.event_id
       flash[:error] = "sorry, couldn't find that object... (404)"
       flash[:sentry_event_id] = event_id if event_id
       redirect_to backend_root_path unless request.path == "/backend" || request.path == "/backend/"
