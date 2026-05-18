0f3efaee3f0c7ffc0ef4d203b3fc28664f2a67bc
24c02 <163450896+24c02@users.noreply.github.com>
2026-05-17 21:34:34 -0400

    null: false on can_process_deletions (no tri-state booleans)

--- db/migrate/20260517000003_add_can_process_deletions_to_backend_users.rb
+++ db/migrate/20260517000003_add_can_process_deletions_to_backend_users.rb
@@ -2,6 +2,6 @@
 
 class AddCanProcessDeletionsToBackendUsers < ActiveRecord::Migration[8.0]
   def change
-    add_column :backend_users, :can_process_deletions, :boolean, default: false
+    add_column :backend_users, :can_process_deletions, :boolean, default: false, null: false
   end
 end
