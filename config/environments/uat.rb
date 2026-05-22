5aa60538a29e74c2c7ed99091ad52d3fd849cfa5
24c02 <163450896+24c02@users.noreply.github.com>
2026-05-22 10:38:31 -0400

    mailer: read APP_HOST from env instead of hardcoding domain

--- config/environments/uat.rb
+++ config/environments/uat.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "auth.hackclub.com" }
+  config.action_mailer.default_url_options = { host: ENV.fetch("APP_HOST") }
 
   # Amazon SES SMTP settings
   config.action_mailer.delivery_method = :smtp
