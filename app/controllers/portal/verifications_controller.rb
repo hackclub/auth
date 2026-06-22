9798020a61ab612ec7c0008bb53b30a69e099815
whatbeato <96067139+whatbeato@users.noreply.github.com>
2026-06-22 17:32:46 +0000

    (cleanup) linted code

--- app/controllers/portal/verifications_controller.rb
+++ app/controllers/portal/verifications_controller.rb
@@ -76,7 +76,7 @@ class Portal::VerificationsController < Portal::BaseController
     setup_persona_step
     if @inquiry_already_completed
       redirect_to_portal_return(status: :pending)
-      return
+      nil
     end
   end
 
