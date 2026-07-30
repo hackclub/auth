e3157b67fcefad3ad2bb0ba4d4d9b51d3f8be19a
lolasanchezz <148605115+lolasanchezz@users.noreply.github.com>
2026-07-30 15:21:50 -0400

    new onboarding pitch

---
+++ app/models/onboarding_scenarios/athena.rb
@@ -0,0 +1,25 @@
+module OnboardingScenarios
+  class Athena < Base
+    def self.slug = "athena"
+
+    def title = "join the athena community!"
+
+    def form_fields = [ :first_name, :last_name, :primary_email, :birthday, :country ]
+
+    def slack_onboarding_flow = :internal_tutorial
+    def slack_channels = chans(:athena_initative, :welcome_to_athena, :athena_announcements)
+
+    def next_action = :slack
+
+    def logo_path = "images/athena/athenaLogo.png"
+    def background_path = "images/athena/bg-img.png"
+    def dialogue_flow
+      [
+        intro: [ template: "tutorial/athena/intro", next: nil ]
+        ]
+    end
+    
+    def dark_mode_background_path = "images/athena/bg-img.png"
+  end
+end
+      
