ad72afe4320915bacccdd4f149d3235997a6dab4
lola sanchez <lola@hackclub.com>
2026-07-31 13:37:13 -0400

    fixed i think

--- app/models/onboarding_scenarios/athena.rb
+++ app/models/onboarding_scenarios/athena.rb
@@ -6,6 +6,8 @@ module OnboardingScenarios
 
     def form_fields = [ :first_name, :last_name, :primary_email, :birthday, :country ]
 
+    def slack_user_type = :full_member
+
     def slack_onboarding_flow = :internal_tutorial
     def slack_channels = chans(:athena_initiative, :welcome_to_athena, :athena_announcements)
 
@@ -15,10 +17,12 @@ module OnboardingScenarios
     def background_path = "images/athena/bg-img.png"
     def dark_mode_background_path = "images/athena/bg-img.png"
 
+    def first_step = :welcome
+
     def dialogue_flow
-      [
-        intro: [ template: "tutorial/athena/intro", next: nil ]
-        ]
+      {
+        welcome: { template: "tutorial/athena/intro", next: nil }
+      }
     end
   end
 end
