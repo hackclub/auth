632c001521af6209902f2ae129a38f35d53e63e4
Kai Pereira <88850028+KaiPereira@users.noreply.github.com>
2026-09-22 15:32:43 -0400

    Shortening text for the Forge card! (#332)
    * shortening text
    
    * changing text again :D

--- app/models/special_app_cards/forge.rb
+++ app/models/special_app_cards/forge.rb
@@ -8,7 +8,7 @@ module SpecialAppCards
 
     def friendly_name = "Forge"
 
-    def tagline = "Design and build hardware projects - get your parts funded and a ticket to Hackaday Supercon 2026 in LA!"
+    def tagline = "Get funding for designing and building hardware projects!"
 
     def icon = "forge.png"
 
