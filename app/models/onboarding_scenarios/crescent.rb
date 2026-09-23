7b984a5be4b37451e6227964b6dfd6ab7ca011ad
ascpixi <44982772+ascpixi@users.noreply.github.com>
2026-09-23 12:50:45 +0100

    lint

--- app/models/onboarding_scenarios/crescent.rb
+++ app/models/onboarding_scenarios/crescent.rb
@@ -9,7 +9,7 @@ module OnboardingScenarios
     def slack_user_type = :full_member
 
     def slack_onboarding_flow = :internal_tutorial
-    def slack_channels = chans(:crescent_bulletin, :crescent_help, :crescent_chat,:identity_help, :help, :welcome_to_hack_club, :slack_guide, :library, :lounge, :welcome, :happenings, :community, :announcements, :news_wire)
+    def slack_channels = chans(:crescent_bulletin, :crescent_help, :crescent_chat, :identity_help, :help, :welcome_to_hack_club, :slack_guide, :library, :lounge, :welcome, :happenings, :community, :announcements, :news_wire)
 
     def first_step = :welcome
 
